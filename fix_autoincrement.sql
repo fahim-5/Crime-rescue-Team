f181f9b5b8df0764b48ba63a76c07abe57e7eb70
Fahim Faysal <fahimbafu@gmail.com>
2025-04-28 04:12:33 +0600

    the addres based user laer is not working logicaly . front ned i okay

--- fix_autoincrement.sql
+++
@@ -1,6 +0,0 @@
--- Fix the AUTO_INCREMENT issue in crime_reports table
--- First, we'll check if there's a record with id=0
-DELETE FROM crime_reports WHERE id = 0;
-
--- Reset the AUTO_INCREMENT value to ensure it starts after the max id
-ALTER TABLE crime_reports AUTO_INCREMENT = 4; 
